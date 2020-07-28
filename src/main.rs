8b92d360406659ad5aaba37cec786d824667ae9a
Charles Neau <charles63500@gmail.com>
2020-07-28 01:34:04 +0100

    ...

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
-use futures::{FutureExt, StreamExt};
+use futures::StreamExt;
 use warp::ws::WebSocket;
 use warp::Filter;
 
+#[derive(Debug, Clone, Copy)]
 struct Manager {}
 
 impl Manager {
     async fn handle_ws(self, ws: WebSocket) {
-        let (tx, mut rx) = ws.split();
+        let (_tx, mut rx) = ws.split();
         // let fut = rx.forward(tx).map(|_| ());
         loop {
             let msg = match rx.next().await {
@@ -23,20 +24,16 @@ impl Manager {
         }
     }
 }
+
 async fn handle_ws(ws: WebSocket) {
     let (tx, mut rx) = ws.split();
     // let fut = rx.forward(tx).map(|_| ());
     loop {
         let msg = match rx.next().await {
-            Some(x) => match x {
-                Ok(x) => match x.to_str() {
-                    Ok(x) => x.to_owned(),
-                    Err(_) => return,
-                },
-                Err(_) => return,
-            },
+            Some(x) => x,
             None => return,
         };
+        let msg = if let Ok(x) = msg { x } else { return };
         println!("{:?}", msg)
     }
 }
@@ -44,10 +41,16 @@ async fn handle_ws(ws: WebSocket) {
 #[tokio::main]
 async fn main() {
     let manager = Manager {};
+    // let manager = Arc::new(manager);
     let home = warp::path("static").and(warp::fs::dir("static"));
-    let wsconn = warp::path("ws")
-        .and(warp::ws())
-        .map(|ws: warp::ws::Ws| ws.on_upgrade(|websocket| handle_ws(websocket)));
+    let wsconn = warp::path("ws").and(warp::ws()).map(|ws: warp::ws::Ws| {
+        let res = ws.on_upgrade(|websocket: WebSocket| {
+            let res = handle_ws(websocket);
+            // let res = manager.handle_ws(websocket);
+            res
+        });
+        res
+    });
     warp::serve(wsconn.or(home))
         .run(([127, 0, 0, 1], 8000))
         .await;
